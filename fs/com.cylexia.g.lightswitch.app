5fd963cffc3ae20c9361ce0c3bb5de5eb9872fc3
sparx104 <chris@cylexia.com>
2015-06-21 02:40:20 +0100

    Feature complete
    Glyde.App.create() now supports being given the source as well as
    loading it
    "window.*" functionality is respected and the window ID changed to force
    to the size specified in the ".app" file
    libcore: added Dict.createFromDictBranch(dict,string)
    manifest updated to provide "glydecommon.js" to background for
    "Glyde.App"

--- fs/com.cylexia.g.lightswitch.app
+++ fs/com.cylexia.g.lightswitch.app
@@ -3,8 +3,8 @@ icon=com.cylexia.g.lightswitch/icon.png;
 script=com.cylexia.g.lightswitch/main.script;
 var=URL=http://master-control/switch.php;
 
-window.chrome=no;
-window.left=10;
-window.top=0;
-window.width=500;
-window.height=50;
+#window.chrome=no;
+#window.left=10;
+#window.top=10;
+#window.width=500;
+#window.height=200;
--- glydecommon.js
+++ glydecommon.js
@@ -10,31 +10,52 @@ var Glyde = {
     if( file.substr( (file.length - 4) ) == ".app" ) {
       file = file.substr( 0, (file.length - 4) );   // remove the ".app"
     }
+    var win = Glyde.App.getWindowDict( o_app );
+console.log( "config: " );
+console.log( o_app );
+    var bounds = { "width": 800, "height": 600 }, frame = {}, rnd = "";
+    var sizable = true;
+    if( Dict.containsKey( win, "chrome" ) ) {
+      if( Dict.valueOf( win, "chrome" ).substr( 0, 2 ) == "no" ) {  // no|none|nothing
+        frame["type"] = "none";
+        sizable = false;
+      }
+    }
+    if( Dict.containsKey( win, "left" ) ) {
+      bounds["left"] = Dict.intValueOf( win, "left" );
+      bounds["top"] = Dict.intValueOf( win, "top" );
+      rnd += Math.random();      // ensures a different ID so previously saved location will not be used
+    }
+    if( Dict.containsKey( win, "width" ) ) {
+      bounds["width"] = Dict.intValueOf( win, "width" );
+      bounds["height"] = Dict.intValueOf( win, "height" );
+      rnd += Math.random();      // ensures a different ID so previously saved location will not be used
+    }
     chrome.app.window.create(
       ('glydert.html#' + file),
       {
-        id: (file + "window"),
-        bounds: {width: 800, height: 600},
-        resizable: false,
-        frame: {
-          //type: "none"
-        }
+        "id": (file + "window" + rnd),
+        "innerBounds": bounds,
+        "resizable": sizable,
+        "frame": frame
       }
     );
   },
   
   App: {
-    create: function( s_file ) {
-      var src = GlueFileManager.readText( s_file );
-      if( src === null ) {
+    create: function( s_file, s_src ) {
+      if( !s_src ) {
+        s_src = GlueFileManager.readText( s_file );
+      }
+      if( s_src === null ) {
         return null;
       }
 			var e, s = 0;
 			var key, value;
 			var data = Dict.create(), vars = Dict.create();
 			Dict.set( data, "_file", s_file );
-			while( (e = src.indexOf( ";", s )) > -1 ) {
-			  var line = src.substr( s, (e - s) ).trim();
+			while( (e = s_src.indexOf( ";", s )) > -1 ) {
+			  var line = s_src.substr( s, (e - s) ).trim();
 			  s = (e + 1);
 				e = line.indexOf( "=" );
 				if( (e > -1) && (line.length > 0) && (line.charAt( 0 ) != "#") ) {
@@ -76,6 +97,10 @@ var Glyde = {
     
     getVarsMap: function( d_app ) {   // Dict
       return Dict.valueOf( d_app, "var_dict" );
+    },
+    
+    getWindowDict: function( d_app ) {
+      return Dict.createFromDictBranch( d_app, "window." );
     }
   }
 };
